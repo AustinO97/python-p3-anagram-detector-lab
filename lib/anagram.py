9f87d444ffa2622214634efed27c04af5c956231
Austin Oblow <austinoblow97@gmail.com>
2024-02-02 12:13:11 -0600

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word_list = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        matched_word = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_list:
+                matched_word.append(word)
+
+        return matched_word
